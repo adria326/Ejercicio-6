3c7b8941a4fc6b3381fc9f6d38f1e9753437e316
Adriana <adriana.murillo@tecnologicoliceocristiano.edu.ec>
2024-08-01 18:32:08 -0500

    Suma

---
+++ 1-SumarLista.py
@@ -0,0 +1,5 @@
+#Instruccion: Dada una lista de numeros, escribe un programa que calcule la sumade todos los elementos en la listay imprime.
+
+lista = [12, 2, 4, 14, 22, 34, 19]
+suma = sum(lista)
+print(suma)
